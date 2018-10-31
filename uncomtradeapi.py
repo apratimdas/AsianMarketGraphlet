b82fceff41c0dcaa0d4a4c724f39ef8cc0d600a9
Apratim Das <apratimdas@gmail.com>
2018-10-31 14:40:33 -0700

    update

--- uncomtradeapi.py
+++ uncomtradeapi.py
@@ -35,20 +35,35 @@ for id in reporteriddata['results']:
 
 idlist.remove(idlist[0])
 
-ctr=0
-for id in reporteriddata['results']:
-    if(id['id'] != '807'):
-        ctr+=1
+# ctr=0
+# for id in reporteriddata['results']:
+#     if(id['id'] != '807'):
+#         ctr+=1
+#     else:
+#         break
+
+# newlist = idlist[ctr:]
+
+
+idlistitr = 0
+
+while idlistitr < len(idlist):
+    idx1 = idlist[idlistitr]
+
+    if(idlistitr+1 < len(idlist)):
+        idx2 = idlist[idlistitr+1]
     else:
-        break
+        idx2 = idx1
+    if(idlistitr+2 < len(idlist)):
+        idx3 = idlist[idlistitr+2]
+    else:
+        idx3 = idx1
+    if(idlistitr+3 < len(idlist)):
+        idx4 = idlist[idlistitr+3]
+    else:
+        idx4 = idx1
 
-newlist = idlist[ctr:]
-
-
-
-for id in newlist:
-    print("id : " + str(id))
-    specificurl = "https://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2016&p=all&rg=all&cc=TOTAL&fmt=csv" + "&r=" + str(id)
+    specificurl = "https://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=1990&p=all&rg=all&cc=TOTAL&fmt=csv" + "&r=" + str(idx1) + "%2C" + str(idx2) + "%2C" + str(idx3) + "%2C" + str(idx4)
     print(specificurl)
     response = requests.get(specificurl)
     decoded_content = response.content.decode('utf-8')
@@ -59,8 +74,27 @@ for id in newlist:
             print(row[9] + ' <-- ' + row[12] + ' : ' + row[-4])
         elif(row[7] == 'Export'):
             print(row[9] + ' --> ' + row[12] + ' : ' + row[-4])
-        else:
-            print(row[9] + ' -- ' + row[12])
+        # else:
+            # print(row[9] + ' -- ' + row[12])
+    idlistitr += 4
+
+
+
+# for id in idlist:
+#     print("id : " + str(id))
+#     specificurl = "https://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2016&p=all&rg=all&cc=TOTAL&fmt=csv" + "&r=" + str(id)
+#     print(specificurl)
+#     response = requests.get(specificurl)
+#     decoded_content = response.content.decode('utf-8')
+#     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+#     my_list = list(cr)
+#     for row in my_list:
+#         if(row[7] == 'Import'):
+#             print(row[9] + ' <-- ' + row[12] + ' : ' + row[-4])
+#         elif(row[7] == 'Export'):
+#             print(row[9] + ' --> ' + row[12] + ' : ' + row[-4])
+#         else:
+#             print(row[9] + ' -- ' + row[12])
 
 
 # response = requests.get(url3)
